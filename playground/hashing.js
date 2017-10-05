71aa051f332b565e52150d344e34a277240d9bb9
CallyB <carole.nissen@gmail.com>
2017-10-05 16:15:27 +0100

    hash user password b4 saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,34 @@
 const { SHA256 } = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-var data = {
-  id: 10
-};
+var password = "123abc!";
 
-var token = jwt.sign(data, "123abc");
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, "123abc");
-console.log(decoded);
+var hashedPassword =
+  "$2a$10$apl6rgteFK0PaGf.8BpWUet3PsUYHaUGNQLWwumws58EKluRjPN22";
 
+bcrypt.compare("123", hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
 //
-// var message = "I am user number 3";
+// var token = jwt.sign(data, "123abc");
+// console.log(token);
+//
+// var decoded = jwt.verify(token, "123abc");
+// console.log(decoded);
+//
+// //
+// // var message = "I am user number 3";
 // var hash = SHA256(message).toString();
 //
 // console.log(`Message: ${message}`);
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const _ = require("lodash");
+const bcrypt = require("bcryptjs");
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -71,6 +72,28 @@ UserSchema.statics.findByToken = function(token) {
     "tokens.access": "auth"
   });
 };
+
+UserSchema.pre("save", function(next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    //user password
+    //call bcrypt.hash then :
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        //user.password = hash
+        user.password = hash;
+        //next
+        next();
+      });
+    });
+
+    //then call to set up new user in postman then check in robomongo (can wipe nonhashed
+    // users first)
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model("User", UserSchema);
 
