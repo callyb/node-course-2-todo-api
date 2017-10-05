38135b8ec3b02109c30c2b6f89b78151f7f54dc6
CallyB <carole.nissen@gmail.com>
2017-10-05 11:51:26 +0100

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const { ObjectID } = require("mongodb");
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todo");
 const { User } = require("./models/user");
+const { authenticate } = require("./middleware/authenticate");
 
 const app = express();
 const port = process.env.PORT;
@@ -101,6 +102,7 @@ app.delete("/todos/:id", (req, res) => {
     });
 });
 
+//edit a todo
 app.patch("/todos/:id", (req, res) => {
   var id = req.params.id;
   // pick() takes an object and pulls name/value pairs off it so only those pairs can be edited
@@ -126,12 +128,34 @@ app.patch("/todos/:id", (req, res) => {
       res.send({ todo });
     })
     .catch(e => {
-      res.status(400).send90;
+      res.status(400).send();
     });
 });
 
+// POST /users
+app.post("/users", (req, res) => {
+  var body = _.pick(req.body, ["email", "password"]);
+  var user = new User(body);
+
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken();
+    })
+    .then(token => {
+      res.header("x-auth", token).send(user);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
+});
+
+app.get("/users/me", authenticate, (req, res) => {
+  res.send(req.user);
+});
+
 app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = { app };
